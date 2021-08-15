af408bfe1609d154d7fd80471506d344f4015a62
Sim2007 <87992154+Sim2007@users.noreply.github.com>
2021-08-15 16:06:17 -0400

    addes some functions

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+function guessCharacter () {
+	
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    letter = game.askForString("Enter a character", 1)
+})
+function startGame () {
+	
+}
+function findLetterIndices (charGuess: string) {
+	
+}
+function guessWord () {
+	
+}
+function checkCharGuessed (charGuess: string) {
+	
+}
+let letter = ""
+game.splash("Welcome to Word Guesser!", "Press A to guess a character, B to guess the word")
+let progress: number[] = []
+console.log("Just testing")
